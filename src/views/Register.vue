7427b32f4fcfe5a3503badf679df3fdc6ae766f8
Anastasia Dubovik <nasthe2@gmail.com>
2020-06-12 00:20:09 +0300

    Add user to database after signing up

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -123,7 +123,7 @@ export default {
     }
   },
   methods: {
-    submitHandler() {
+    async submitHandler() {
       if (this.$v.$invalid) {
         this.$v.$touch();
         return;
@@ -133,8 +133,12 @@ export default {
         password: this.password,
         name: this.name
       };
-      console.log(formData);
-      this.$router.push("/");
+
+      try {
+        await this.$store.dispatch("register", formData);
+        this.$router.push("/");
+        // eslint-disable-next-line
+      } catch (e) {}
     }
   }
 };
