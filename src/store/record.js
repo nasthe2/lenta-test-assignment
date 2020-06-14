aac8c5277986323b731f6c065247ba2b0f54e379
Anastasia Dubovik <nasthe2@gmail.com>
2020-06-14 16:45:21 +0300

    Adjust fetching record by id if admin mode got enabled

--- src/store/record.js
+++ src/store/record.js
@@ -40,18 +40,11 @@ export default {
         } else {
           const records = (await firebase.database().ref(`/users/${uid}/records`).once("value")).val() || {};
 
-          console.log(Object.keys(records).map(key => ({
-            ...records[key], 
-            id: key
-          })));
-
           return Object.keys(records).map(key => ({
             ...records[key], 
             id: key
           }));
         }
-
-        
 
       } catch (e) {
         commit("setError", e);
@@ -61,11 +54,48 @@ export default {
     async fetchRecordById({dispatch, commit}, id) {
       try {
         const uid = await dispatch("getUid");
-        const record = (await firebase.database().ref(`/users/${uid}/records`).child(id).once("value")).val() || {};
-        return { 
-          ...record,
-          id
-        };
+        const isAdmin = (await firebase.database().ref(`/users/${uid}/info`).once("value")).val().admin;
+
+        if (isAdmin) {
+
+          const adminRecords = (await firebase.database().ref(`/users/`).once("value")).val() || {};
+
+          let allRecords = [];
+          
+          Object.keys(adminRecords).forEach(key => {            
+            if (adminRecords[key]["records"]) {
+              Object.keys(adminRecords[key]["records"]).forEach(newKey => {
+
+                allRecords.push({
+                  ...adminRecords[key]["records"][newKey],
+                  newKey
+                });
+                
+              });             
+            }        
+          });
+
+          let recordId;
+          
+          allRecords.forEach((key, i) => {
+            if (key.newKey === id) {
+              recordId = i;
+            }
+          });
+
+          return {
+            ...allRecords[recordId],
+            id
+          };
+
+        } else {
+          const record = (await firebase.database().ref(`/users/${uid}/records`).child(id).once("value")).val() || {};
+          return { 
+            ...record,
+            id
+          }; 
+        }
+        
       } catch (e) {
         commit("setError", e);
         throw e;
